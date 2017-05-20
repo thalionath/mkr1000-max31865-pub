7a3680359b76590fdc5d9725175c3b9a79efe801
Mario Gruber <mario.gruber@space.unibe.ch>
2017-05-20 10:17:24 +0200

    adds RTC timesync with NTP server

--- mkr1000-max31865-pub.ino
+++ mkr1000-max31865-pub.ino
@@ -1,6 +1,8 @@
 
 #include <SPI.h>
 #include <WiFi101.h>
+#include <WiFiUdp.h>
+#include <RTCZero.h>
 #include "config.h"
 
 // The <Arduino.h> header defines max and min macros.
@@ -18,13 +20,15 @@ char server[] = "192.168.2.33"; // name address for Google (using DNS)
 
 static constexpr auto port = 8080;
 
+RTCZero rtc;
+
 // Initialize the Ethernet client library
 // with the IP address and port of the server
 // that you want to connect to (port 80 is default for HTTP):
 WiFiClient client;
 
-namespace time
-{
+namespace time {
+
     using ms_t = uint32_t;
 
     static ms_t now()
@@ -42,7 +46,102 @@ namespace time
 
     static ms_t elapsed_since(ms_t t0)
     {
-        return elapsed(t0, now());
+        return elapsed(t0, time::now());
+    }
+
+    /**
+     * © Francesco Potortì 2013 - GPLv3 - Revision: 1.13
+     *
+     * Send an NTP packet and wait for the response, return the Unix time
+     *
+     * To lower the memory footprint, no buffers are allocated for sending
+     * and receiving the NTP packets.  Four bytes of memory are allocated
+     * for transmision, the rest is random garbage collected from the data
+     * memory segment, and the received packet is read one byte at a time.
+     * The Unix time is returned, that is, seconds from 1970-01-01T00:00.
+     */
+    unsigned long ntpUnixTime()
+    {
+        WiFiUDP udp;
+
+        static int udpInited = udp.begin(123); // open socket on arbitrary port
+
+        const char timeServer[] = "pool.ntp.org";  // NTP server
+
+        // Only the first four bytes of an outgoing NTP packet need to be set
+        // appropriately, the rest can be whatever.
+        const long ntpFirstFourBytes = 0xEC0600E3; // NTP request header
+
+        // Fail if WiFiUdp.begin() could not init a socket
+        if( ! udpInited )
+        {
+            return 0;
+        }
+
+        // Clear received data from possible stray received packets
+        udp.flush();
+
+        // Send an NTP request
+        if (! (udp.beginPacket(timeServer, 123) // 123 is the NTP port
+            && udp.write((byte *)&ntpFirstFourBytes, 48) == 48
+            && udp.endPacket()))
+        {
+            return 0; // sending request failed
+        }
+
+        // Wait for response; check every pollIntv ms up to maxPoll times
+
+        const int pollIntv = 150;   // poll every this many ms
+        const byte maxPoll = 15;    // poll up to this many times
+        int pktLen;				    // received packet length
+
+        for( byte i=0; i < maxPoll; i++ )
+        {
+            if( (pktLen = udp.parsePacket()) == 48 )
+            {
+                break;
+            }
+
+            delay(pollIntv);
+        }
+        
+        if( pktLen != 48 )
+        {
+            return 0; // no correct packet received
+        }
+
+        // Read and discard the first useless bytes
+        // Set useless to 32 for speed; set to 40 for accuracy.
+        constexpr byte useless = 40;
+
+        for( byte i = 0; i < useless; ++i )
+        {
+            udp.read();
+        }
+
+        // Read the integer part of sending time
+        unsigned long time = udp.read();	// NTP time
+
+        for( byte i = 1; i < 4; i++ )
+        {
+            time = time << 8 | udp.read();
+        }
+
+        // Round to the nearest second if we want accuracy
+        // The fractionary part is the next byte divided by 256: if it is
+        // greater than 500ms we round to the next second; we also account
+        // for an assumed network delay of 50ms, and (0.5-0.05)*256=115;
+        // additionally, we account for how much we delayed reading the packet
+        // since its arrival, which we assume on average to be pollIntv/2.
+        time += (udp.read() > 115 - pollIntv/8);
+
+        // Discard the rest of the packet
+        udp.flush();
+
+        // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+        constexpr auto const seventyYears = 2208988800ul;
+
+        return time - seventyYears;		// convert NTP time to Unix time
     }
 }
 
@@ -70,6 +169,9 @@ static void request()
         client.println();
 
         last_request = time::now();
+
+        Serial.print("now = ");
+        Serial.println(rtc.getEpoch());
 
         state = response;
     }
@@ -114,6 +216,23 @@ void setup()
         // wait for serial port to connect. Needed for native USB port only
     }
 
+    rtc.begin();
+
+    Serial.print("RTC: ");
+    
+    Serial.print(rtc.getDay());
+    Serial.print(".");
+    Serial.print(rtc.getMonth());
+    Serial.print(".");
+    Serial.print(rtc.getYear());
+    Serial.print(" ");
+    Serial.print(rtc.getHours());
+    Serial.print(":");
+    Serial.print(rtc.getMinutes());
+    Serial.print(":");
+    Serial.print(rtc.getSeconds());
+    Serial.println(":");
+
     // check for the presence of the shield:
     if( WiFi.status() == WL_NO_SHIELD )
     {
@@ -144,9 +263,41 @@ static bool connectToWifi()
 
     Serial.println("Connected to wifi");
 
-    printWiFiStatus();    
+    printWiFiStatus();
 
-    return true;
+    while( status == WL_CONNECTED )
+    {
+        auto const unixTime = time::ntpUnixTime();
+
+        if( unixTime )
+        {
+            rtc.setEpoch(unixTime);
+
+            Serial.print("time: ");
+            Serial.println(unixTime);
+
+            // subtract seventy years:
+            auto const epoch = unixTime;
+
+            auto const hour   = (epoch  % 86400) / 3600;
+            auto const minute = (epoch  %  3600) /   60;
+            auto const second = (epoch  %    60)       ;
+
+            Serial.print(hour);
+            Serial.print(":");
+            Serial.print(minute);
+            Serial.print(":");
+            Serial.print(second);
+
+            return true;
+        }
+        else
+        {
+            Serial.println("failed to fetch time from NTP server");
+        }
+    }
+
+    return false;
 }
 
 void loop()
