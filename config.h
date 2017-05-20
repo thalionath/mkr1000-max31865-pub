1423cd0ce609cbf96b89a8433caafd93eb9c71d5
Mario Gruber <mario.gruber@space.unibe.ch>
2017-05-20 08:40:03 +0200

    initial

---
+++ config.h
@@ -0,0 +1,9 @@
+
+#ifndef _CONFIG_H_INCLUDED__
+#define _CONFIG_H_INCLUDED__
+
+char ssid[] = ""; //  your network SSID (name)
+char pass[] = "";  // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;           // your network key Index number (needed only for WEP)
+
+#endif // _CONFIG_H_INCLUDED__
---
+++ mkr1000-max31865-pub.ino
@@ -0,0 +1,182 @@
+
+#include <SPI.h>
+#include <WiFi101.h>
+#include "config.h"
+
+// The <Arduino.h> header defines max and min macros.
+// It really shouldn't do that.
+#undef max
+#undef min
+
+#include <limits>
+
+int status = WL_IDLE_STATUS;
+// if you don't want to use DNS (and reduce your sketch size)
+// use the numeric IP instead of the name for the server:
+//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+char server[] = "192.168.2.33"; // name address for Google (using DNS)
+
+static constexpr auto port = 8080;
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+WiFiClient client;
+
+namespace time
+{
+    using ms_t = uint32_t;
+
+    static ms_t now()
+    {
+        return millis();
+    }
+
+    static constexpr ms_t elapsed(ms_t t0, ms_t t1)
+    {
+        return t1 < t0 ? (std::numeric_limits<ms_t>::max() - t0 + 1) + t1 : t1 - t0;
+    }
+
+    static_assert(elapsed(0, 0) == 0, "");
+    static_assert(elapsed(0xFFFFFFFFu, 0) == 1, "overflow");
+
+    static ms_t elapsed_since(ms_t t0)
+    {
+        return elapsed(t0, now());
+    }
+}
+
+static void request();
+static void response();
+static void wait();
+
+using State = void (*)();
+
+static State state = request;
+static auto last_request = time::now();
+
+static void request()
+{
+    Serial.println("\nStarting connection to server...");
+
+    // if you get a connection, report back via serial:
+    if( client.connect(server, port) )
+    {
+        Serial.println("connected to server");
+        // Make a HTTP request:
+        client.println("GET /search?q=arduino HTTP/1.1");
+        client.println("Host: www.google.com");
+        client.println("Connection: close");
+        client.println();
+
+        last_request = time::now();
+
+        state = response;
+    }
+}
+
+static void response()
+{
+    // if there are incoming bytes available
+    // from the server, read them and print them:
+    while( client.available() )
+    {
+        char c = client.read();
+        Serial.write(c);
+    }
+
+    // if the server's disconnected, stop the client:
+    if( ! client.connected() )
+    {
+        Serial.println();
+        Serial.println("disconnecting from server.");
+        client.stop();
+
+        state = wait;
+    }
+}
+
+static void wait()
+{
+    if( time::elapsed_since(last_request) >= 1000 )
+    {
+        state = request;
+    }
+}
+
+void setup()
+{
+    //Initialize serial and wait for port to open:
+    Serial.begin(9600);
+
+    while( ! Serial )
+    {
+        // wait for serial port to connect. Needed for native USB port only
+    }
+
+    // check for the presence of the shield:
+    if( WiFi.status() == WL_NO_SHIELD )
+    {
+        Serial.println("WiFi shield not present");
+       
+        for(;;) {}
+    }    
+}
+
+static bool connectToWifi()
+{
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    auto const t0 = time::now();
+
+    while( status != WL_CONNECTED )
+    {
+        if( time::elapsed_since(t0) > 10000 )
+        {
+            Serial.println("Connection attempt failed");
+            return false;
+        }        
+    }
+
+    Serial.println("Connected to wifi");
+
+    printWiFiStatus();    
+
+    return true;
+}
+
+void loop()
+{
+    // attempt to connect to WiFi network:
+    while( status != WL_CONNECTED )
+    {
+        if( connectToWifi() )
+        {
+            state = request;
+        }
+    }
+
+    state();
+}
+
+void printWiFiStatus()
+{
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+
+    // print your WiFi shield's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+
+    // print the received signal strength:
+    long rssi = WiFi.RSSI();
+    Serial.print("signal strength (RSSI):");
+    Serial.print(rssi);
+    Serial.println(" dBm");
+}
